335a3a2f63b405ba947ffd2d8f3bc62790b5dfab
mamjerez <correomam@gmail.com>
2019-05-28 16:38:25 +0200

    Grid funciona OK.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  cols: number;
+  rows: number;
+  text: string;
+  border: string;
+ }
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+   tiles = [
+    {text: 'Información', cols: 1, rows: 1, color: '#F2F2F2'},
+    {text: '7:00 a 8:00', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '8:00 a 9:00', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '9:00 a 10:00', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '10:00 a 11:00', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '11:00 a 12:00', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '12:00 a 13:00', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '13:00 a 14:00', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '14:00 a 15:00', cols: 1, rows: 1, color: '#ADD8E6'},
+
+  ];
+
+  tilesFooter = [
+    {text: 'Resumen', cols: 1, rows: 1, color: '#F2F2F2'},
+    {text: '1', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '1', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '3', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '30', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '30', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '30', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '30', cols: 1, rows: 1, color: '#ADD8E6'},
+    {text: '30', cols: 1, rows: 1, color: '#ADD8E6'},
+
+  ];
   title = 'kamizibai';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatGridListModule,
+  MatToolbarModule,
+} from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -12,6 +17,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatGridListModule,
+    MatToolbarModule,
     BrowserAnimationsModule
   ],
   providers: [],
