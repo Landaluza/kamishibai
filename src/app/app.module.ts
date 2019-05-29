8e6d3d4c74f02a0a20d8afa10b99760bcdbf72ff
mamjerez <correomam@gmail.com>
2019-05-29 16:28:14 +0200

    Actualizo a Angular-material 8.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  MatGridListModule,
-  MatToolbarModule,
-} from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
