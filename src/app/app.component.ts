8b8b93b81d2d5de5dc4c615459c9011bb3236b42
mamjerez <correomam@gmail.com>
2019-06-07 18:49:11 +0200

    Funciona horaControl7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,11 @@ import {
   ViewChild,
   ElementRef,
   OnInit,
+  Output,
+  EventEmitter
 } from '@angular/core';
+
+import * as moment from 'moment';
 
 export interface Tile {
   cols: number;
@@ -21,6 +25,8 @@ export interface Tile {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   @ViewChild('overlay0', { static: false }) card0: ElementRef;
   @ViewChild('overlay1', { static: false }) card1: ElementRef;
@@ -30,47 +36,47 @@ export class AppComponent {
 
  verBoton7 = true;
  verBoton8 = true;
+ horaControl7: string;
 
   constructor(private renderer: Renderer2) {
 
-  }
+      }
 
-  tiles = [
-    { text: '7:00 a 8:00', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '8:00 a 9:00', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '9:00 a 10:00', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '10:00 a 11:00', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '11:00 a 12:00', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '12:00 a 13:00', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '13:00 a 14:00', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '14:00 a 15:00', cols: 1, rows: 1, color: '#ADD8E6' }
-  ];
+  // tiles = [
+  //   { text: '7:00 a 8:00', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '8:00 a 9:00', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '9:00 a 10:00', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '10:00 a 11:00', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '11:00 a 12:00', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '12:00 a 13:00', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '13:00 a 14:00', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '14:00 a 15:00', cols: 1, rows: 1, color: '#ADD8E6' }
+  // ];
 
-  tilesFooter = [
-    { text: '1', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '2', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '3', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '4', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '5', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '6', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '7', cols: 1, rows: 1, color: '#ADD8E6' },
-    { text: '8', cols: 1, rows: 1, color: '#ADD8E6' }
-  ];
-  title = 'kamizibai';
+  // tilesFooter = [
+  //   { text: '1', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '2', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '3', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '4', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '5', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '6', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '7', cols: 1, rows: 1, color: '#ADD8E6' },
+  //   { text: '8', cols: 1, rows: 1, color: '#ADD8E6' }
+  // ];
+  title = 'Tablero kamizibai';
+
 
   onClickHecho(numCard: number) {
-
-    // this.renderer.removeChild(window, this.boton7.nativeElement);
-    // this.renderer.removeClass(this.boton7, 'boton7');
-    switch (numCard) {
+      switch (numCard) {
       case 0:
         this.renderer.setStyle(this.card0.nativeElement, 'backgroundColor', 'green');
         this.renderer.setStyle(this.hora7.nativeElement, 'backgroundColor', 'green');
         this.renderer.setStyle(this.hora7.nativeElement, 'padding', '15px');
-        this.renderer.setStyle(this.hora7.nativeElement, 'text', 'Hora control');
         this.verBoton7 = false;
+        this.horaControl7 = moment().format('LT');
+        // alert('El control se ha hecho a  las ' + this.horaControl7);
 
-        console.log('hora7: ' + this.hora7.nativeElement);
+
         break;
       case 1:
         this.renderer.setStyle(this.card1.nativeElement, 'backgroundColor', 'green');
