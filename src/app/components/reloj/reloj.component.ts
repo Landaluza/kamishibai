08bf9796d47958d1f3caf7d55b4e298fd1797f37
mamjerez <correomam@gmail.com>
2019-06-09 10:26:50 +0200

    Cambio distribución mat-gris-list de cabecera.

--- src/app/components/reloj/reloj.component.ts
+++ src/app/components/reloj/reloj.component.ts
@@ -1,3 +1,4 @@
+// https://codepen.io/jaystephens3/pen/qoKgn
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,35 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reloj.component.css']
 })
 export class RelojComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    window.onload = function() {
-      clock();
-      function clock() {
-        const now = new Date();
-        const TwentyFourHour = now.getHours();
-        let hour = now.getHours();
-        let min: any = now.getMinutes();
-        const sec = now.getSeconds();
-        let mid = 'PM';
-        if (min < '10') {
-          min = '0' + min;
-        }
-        if (hour > 12) {
-          hour = hour - 12;
-        }
-        if (hour === 0) {
-          hour = 12;
-        }
-        if (TwentyFourHour < 12) {
-           mid = 'AM';
-        }
-        document.getElementById('currentTime').innerHTML =     hour + ':' + min + ':' + sec + ' ' + mid ;
-        setTimeout(clock, 1000);
-        }
-    }
+    clock();
   }
+}
 
+function clock() {
+  const now = new Date();
+  const TwentyFourHour = now.getHours();
+  let hour = now.getHours();
+  let min: any = now.getMinutes();
+  let sec: any = now.getSeconds();
+  if (sec < 10) {
+    sec = '0' + sec;
+  }
+  let mid = 'PM';
+  if (min < 10) {
+    min = '0' + min;
+  }
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+  if (hour === 0) {
+    hour = 12;
+  }
+  if (TwentyFourHour < 12) {
+    mid = 'AM';
+  }
+  document.getElementById('currentTime').innerHTML =
+    hour + ':' + min + ':' + sec + ' ' + mid;
+  setTimeout(clock, 1000);
 }
