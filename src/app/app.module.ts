caed6d831aa23d5d161af04ab25957f4c6a48c7e
mamjerez <correomam@gmail.com>
2019-06-21 17:19:20 +0200

    Creo material.module para tener todas las importaciones de modulos de Angular Material necesarios en este modulo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatToolbarModule } from '@angular/material/toolbar';
+// Para usar Angular Material.
+import {MaterialModule} from './material.module';
 
 // Para que las fechas y números esten en español.
 import { LOCALE_ID} from '@angular/core';
 // https://angular.io/guide/i18n#i18n-pipes
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
+
 import { PruebaPipe } from './pipes/prueba.pipe';
 import { CountdownComponent } from './components/countdown/countdown.component';
 import { RelojComponent } from './components/reloj/reloj.component';
@@ -39,11 +39,9 @@ registerLocaleData(localeEs, 'es');
     RowCardsComponent
     ],
   imports: [
+    MaterialModule,
     BrowserModule,
-    AppRoutingModule,
-    MatGridListModule,
-    MatToolbarModule,
-    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'es'}
